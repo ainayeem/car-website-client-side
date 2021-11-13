a8e1af78dc4d592ee13c1cff2a5d89680156187b
ainayeem <ain.nayeem1999@gmail.com>
2021-11-14 00:25:25 +0600

    first commit

---
+++ src/Pages/Firebase/firebaseConfiq.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCntOL7MRiauGSre0Uioiyxm438d96KZUo",
+  authDomain: "ass12-dbd36.firebaseapp.com",
+  projectId: "ass12-dbd36",
+  storageBucket: "ass12-dbd36.appspot.com",
+  messagingSenderId: "704187114842",
+  appId: "1:704187114842:web:def7565f499e575a907f63",
+};
+export default firebaseConfig;
+
+// apiKey: process.env.REACT_APP_API_KEY,
+// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+// projectId: process.env.REACT_APP_PROJECT_ID,
+// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// appId: process.env.REACT_APP_APP_ID,
